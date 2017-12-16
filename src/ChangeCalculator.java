a7b7c1ddcffb617ba7597149f49960387c51a992
dylanmafong <league@wts-im-17.attlocal.net>
2017-12-15 16:56:46 -0800

    I did change calculater and I did simple calculater

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -1,7 +1,6 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
 import javax.swing.JOptionPane;
 
 /*
@@ -13,18 +12,21 @@ public class ChangeCalculator {
 
 	public static void main(String[] args) {
 
+		String nickels = JOptionPane.showInputDialog("how many nickels do you have?");
+		int nickel =	Integer.parseInt(nickels);
+		String dimes = JOptionPane.showInputDialog("how many dimes do you have?");
+	int dime = Integer.parseInt(dimes);
+		String quarters = JOptionPane.showInputDialog("how many quartetrs do you have?");
+		int quarter =Integer.parseInt(quarters);
+		JOptionPane.showMessageDialog(null, "you have " + (nickel * 5 + dime * 10 + quarter * 25) + " cents");
 		// Ask the user how many nickels they have
-
-		// Convert their answer to an int using Integer.parseInt()
-
 		// Ask the user how many dimes they have, and convert their answer
 
 		// Ask the user how many quarters they have, and convert their answer
 
-		// Calculate how much money the user has and save it in a double variable 
+		// Calculate how much money the user has and save it in a double variable
 
 		// Tell the user how much money they have
 
 	}
 }
-
--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,25 +1,47 @@
 
-
 import javax.swing.JOptionPane;
-
 public class SimpleCalculator {
 	public static void main(String[] args) {
-
-		// 1. Get 2 numbers from the user and convert them to integer.
-
+		// 1. Get 2 numbers from the user and convert them to integer.st
+		String number = JOptionPane.showInputDialog("give me a number");
+		int numbers = Integer.parseInt(number);
+		String number2 = JOptionPane.showInputDialog("give me a number");
+		int numbers2 = Integer.parseInt(number);
+		int operation = JOptionPane.showOptionDialog(null, "calculater", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "multiply", "divide", "add", "subtract" }, null);
+		System.out.println(operation);
+		if (operation == 2) {
+			addition(numbers, numbers2);
+		}
+		if (operation == 0) {
+			multiply(numbers, numbers2);
+		}
+		if (operation == 1) {
+			division(numbers, numbers2);
+		}
+		if (operation == 3) {
+			subtraction(numbers, numbers2);
+		}
+	}
+	public static void addition(int a, int b) {
+		JOptionPane.showMessageDialog(null, a + " + " + b + " = " + (a + b));
+	}
+	public static void multiply(int a, int b) {
+		JOptionPane.showMessageDialog(null, a + " X " + b + " = " + (a * b));
+	}
+	public static void division(int a, int b) {
+		JOptionPane.showMessageDialog(null, a + " / " + b + " = " + (a / b));
+	}
+		public static void subtraction(int a, int b) {
+			JOptionPane.showMessageDialog(null, a + " - " + b + " = "  + (a - b));
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
-				null);
-
-		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-
+		// 5. Call the methods created in steps 3 and 4 to perform the appropriate
+		// operation.
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 	}
-
 	// 3. Create method for addition operation.
-	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
-	//    HINT: use 'static void add(int num1, num2) { ... }
-
+	// Method should create pop-up that shows equation and solution, eg. 45 + 25 =
+	// 70.
+	// HINT: use 'static void add(int num1, num2) { ... }
 	// 4. Create similar methods for subtraction, multiplication and division.
 }
